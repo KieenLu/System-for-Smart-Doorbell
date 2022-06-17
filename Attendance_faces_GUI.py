36ace9cb841383b472449575e038a87f6e14225f
Lư Trung Kiên <71935436+KieenLu@users.noreply.github.com>
2022-06-17 11:07:52 +0700

    Update Attendance_faces_GUI.py

--- Attendance_faces_GUI.py
+++ Attendance_faces_GUI.py
@@ -21,14 +21,6 @@ label.grid(row=0, column=0)
 cap = cv2.VideoCapture(1)
 
 
-db = mysql.connector.connect(host='smartdoorbellsystem.cynisqrgpez0.ap-southeast-1.rds.amazonaws.com',
-                     user='kienlu',
-                     password='12345678',
-                     database='Attendance_faces',)
-
-
-scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
-         "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
 
 creds = ServiceAccountCredentials.from_json_keyfile_name("attendance-faces-e054fb69a2a3.json", scope)
 client = gspread.authorize(creds)
